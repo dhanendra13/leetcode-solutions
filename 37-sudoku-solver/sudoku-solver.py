5f70429180ed8af32c9747f49c7145926a42594c
dhanendra13 <dixitdhanendra324@gmail.com>
2026-06-27 10:18:01 +0530

    Time: 2122 ms (70.49%) | Memory: 12.3 MB (88.53%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solveSudoku(self, board):
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty = []
+
+        # Initialize sets
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    empty.append((i, j))
+                else:
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    boxes[(i // 3) * 3 + j // 3].add(board[i][j])
+
+        def backtrack(idx):
+            if idx == len(empty):
+                return True
+
+            r, c = empty[idx]
+            b = (r // 3) * 3 + c // 3
+
+            for num in "123456789":
+                if num not in rows[r] and num not in cols[c] and num not in boxes[b]:
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[b].add(num)
+
+                    if backtrack(idx + 1):
+                        return True
+
+                    board[r][c] = "."
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[b].remove(num)
+
+            return False
+
+        backtrack(0)
